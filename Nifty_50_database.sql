3be42143bacd5cb0c668ada57a1a452cb67adfeb
Nihal-997 <nihalchapekar@gmail.com>
2025-05-17 14:24:56 +0530

    Add files via upload
    Sql databases

---
+++ Nifty_50_database.sql
@@ -0,0 +1,882 @@
+CREATE TABLE Adanient (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades BIGINT,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE AdaniPorts (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE AppolloHosp (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Asianpaint (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE AxisBank (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE BajajAuto (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE BajajFinsv (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE BajFinance (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Bel (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE BhartiArtl (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Cipla (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE CoalIndia (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE DrReddy (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Eichermot (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Eternal (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Grasim (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE HclTech (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE HdfcBank (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE HdfcLife (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Heromotoco (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Hindalco (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Hindunilvr (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE IciciBank (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Indusindbk (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Infy (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Itc (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Jiofin (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE JswSteel (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE KotakBank (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE LT (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE M_M (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Maruti (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Nestleind (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Ntpc (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Ongc (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE PowerGrid (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Reliance (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE SbiLife (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Sbin (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE ShriRamFin (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE SunPharma (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE TataConsume (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE TataMotors (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE TataSteel (
+   Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Tcs (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Techm (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+CREATE TABLE Titan (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Trent (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Ultracemco (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+
+CREATE TABLE Wipro (
+    Date DATE,
+    Series VARCHAR(10),
+    Open float,
+    High float,
+    Low float,
+    Prev_Close float,
+    LTP float,
+    Close float,
+    VWAP float,
+    Fiftytwo_W_H float,
+    Fiftytwo_W_L float,
+    Volume float,
+    Value float,
+    No_of_Trades float,
+    Stock_Name VARCHAR(50)
+);
+ select * from Wipro
